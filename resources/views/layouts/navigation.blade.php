202b130ab05ef9b36890f29e0f27e1d9921eaacd
Fadhail <mochammadfadhail2@gmail.com>
2023-12-20 08:44:05 +0700

    perbaikan

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -83,8 +83,8 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('beranda')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('beranda')" :active="request()->routeIs('beranda')">
+                {{ __('Beranda') }}
             </x-responsive-nav-link>
         </div>
 
